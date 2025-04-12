8496a72027fab2d854b45ea1378eaba21524966b
joyvin <joymendonca710@gmail.com>
2025-04-13 03:36:32 +0530

    minor change

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,5 +1,11 @@
 import MCQ from "~/components/MCQ";
+import MCQQuiz from "~/components/MCQQuiz";
 
 export default function QuizPage() {
-  return <MCQ />;
+  return (
+    <>
+      {/* <MCQQuiz contentId="content-123" /> */}
+      <MCQ />
+    </>
+  );
 }
