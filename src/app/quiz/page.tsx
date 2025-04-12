46a117ab1d854c99c5686083fa64bd46cc0a9165
AlvinDHacker <alvindsouza2204@gmail.com>
2025-04-13 03:29:37 +0530

    CHeck this

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,7 +1,11 @@
+import MCQ from "~/components/MCQ";
 import MCQQuiz from "~/components/MCQQuiz";
 
 export default function QuizPage() {
   return (
-    <MCQQuiz contentId="content-123" />
+    <>
+      <MCQQuiz contentId="content-123" />
+      <MCQ />
+    </>
   );
 }
