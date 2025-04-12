1c38ee4669b8c59152ef10688ff232946c693903
AlvinDHacker <alvindsouza2204@gmail.com>
2025-04-12 16:12:43 +0530

    Minor Fix

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,7 +9,7 @@ export default function SignPage() {
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <GalleryVerticalEnd className="size-4" />
           </div>
-          Test
+          QConnect
         </a>
         <SignUpForm />
       </div>
